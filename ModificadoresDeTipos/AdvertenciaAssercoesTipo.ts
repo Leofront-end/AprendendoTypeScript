a5dfe3d8f92aa1ec868fe778a28d89d1a8381c7a
Leofront-end <ls171258@gmail.com>
2025-10-16 12:48:46 -0300

    feat: advertencia de asserções de tipo

---
+++ ModificadoresDeTipos/AdvertenciaAssercoesTipo.ts
@@ -0,0 +1,8 @@
+const seasonCounts2 = new Map([
+    [5, "Broad City"],
+    [6, "Community"]
+])
+
+
+// const knownValue2 = seasonCounts.get("I love lucy")!
+// console.log(knownValue2.toUpperCase());
