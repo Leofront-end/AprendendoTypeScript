e4c19c6145ff819a4c91e3870dfdb6bc4200289d
Leofront-end <ls171258@gmail.com>
2025-10-22 15:10:27 -0300

    feat: objeto somente leitura

---
+++ ModificadoresDeTipos/ObjetosSomenteLeitura.ts
@@ -0,0 +1,25 @@
+function describePreference(preference: "maybe" | "no" | "yes") {
+    switch (preference) {
+        case "maybe": return "I suppose ..."
+        case "no": return "No thanks"
+        case "yes": return "Yes please"
+    }
+}
+
+const preferenceMutable = {
+    movie: "maybe",
+    standup: "yes"
+}
+
+describePreference(preferenceMutable.movie)
+
+preferenceMutable.movie = "no"
+
+const preferenceReadOnly = {
+    movie: "maybe",
+    standup: "yes"
+} as const
+
+describePreference(preferenceReadOnly.movie)
+
+preferenceReadOnly.movie = 'no'
